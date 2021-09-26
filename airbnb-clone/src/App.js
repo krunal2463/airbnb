314442a9070179a00a871e2c3e63dce600e1f7f7
Krutarth <krunalpatel199360@gmail.com>
2021-09-26 12:40:31 +0200

    initial

---
+++ airbnb-clone/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Home from './Home';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <Home />
+    </div>
+  );
+}
+
+export default App;
+
+
+/* Home Component */
+    /* Header */
+    /* Banner */
+        /* Search */
+    /* Cards*/
+    /* Footer */
+
+    /* SearchPage */
+          /* Header */
+         /* .... */

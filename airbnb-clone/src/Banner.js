88baa2afc02b828e17802771756a21ee69b6e207
Krutarth <krunalpatel199360@gmail.com>
2021-09-28 18:46:53 +0200

    Banner Info and Search + css

--- airbnb-clone/src/Banner.js
+++ airbnb-clone/src/Banner.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Banner.css';
+import Button from '@material-ui/core/Button'; 
 
 function Banner() {
     return (
         <div className="banner">
-            <h1>Hello New Banner</h1>
+            <div className="banner__search">
+                <Button className="banner__searchButton">Search Dates</Button>
+            </div>
+         <div className="banner__info">
+             <h1>Get out and stretch your imagination</h1>
+             <h5>Plan a different kind of getway to uncover the hidden gems near you.</h5>
+             <Button variant="outlined">Explore Nearby</Button>
+         </div>
         </div>
     )
 }

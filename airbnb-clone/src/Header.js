62c9f3e6dfbc3e3892b7d82b72e1cb7cc69582df
Krutarth <krunalpatel199360@gmail.com>
2021-10-16 14:22:30 +0200

    Final Changes

--- airbnb-clone/src/Header.js
+++ airbnb-clone/src/Header.js
@@ -17,7 +17,7 @@ function Header() {
           
 
             <div className="header__center">
-                <input type="text" />
+                <input type="text" placeholder='Start your search' />
                 <SearchIcon />
             </div>
 
--- airbnb-clone/src/SearchPage.js
+++ airbnb-clone/src/SearchPage.js
@@ -7,12 +7,12 @@ function SearchPage() {
     return (
         <div className="searchPage">
             <div className="searchPage__info">
-            <p>62 Stays | 26 August to 30 August | 2 Guest</p>
-            <h1>Stays nearby</h1>
-            <Button variant="outlined">Cancellation Flexibility</Button>
+            <p>300 + stays</p>
+            <h1>Stays in Prague</h1>
+            <Button variant="outlined">Free cancellation </Button>
             <Button variant="outlined">Type of place</Button>
             <Button variant="outlined">Price</Button>
-            <Button variant="outlined">Rooms and beds</Button>
+            <Button variant="outlined">Instant Book</Button>
             <Button variant="outlined">More filters</Button>
             </div>
             <SearchResult 
